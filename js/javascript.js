c57add2addaa8a23f2844e32cbffc09357d9c79e
nhanbs1997 <nhanbs1997@gmail.com>
2018-10-30 12:23:23 +0700

    Completed Cột Điện Kitchen (80% bằng code thuần)

--- js/javascript.js
+++ js/javascript.js
@@ -12,12 +12,25 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
-      }
+  if (n > slides.length) 
+  {
+    slideIndex = 1
+  }    
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) 
+  {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
 
-      slides[slideIndex-1].style.display = "block";  
-
-    }
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
